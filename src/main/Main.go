ebb61797381223c10586123825e6ba4c7e1da360
Eray Mercan <eraymercan616@gmail.com>
2021-06-10 00:34:06 +0300

    Update Main.go

--- src/main/Main.go
+++ src/main/Main.go
@@ -1,13 +1,25 @@
 package main;
 
 import "fmt";
-import "strings";
+//import "strings";
 
 
 
 func main() {
 
 	fmt.Println("--Eray Mercan--\neraymercan616@gmail.com")
-	
+	fmt.Println("--komutlar için yazın, yardım")
+
+	for true{
+
+		var command string;
+		fmt.Scanln(&command);
+		switch command {
+		case "yardım":
+			fmt.Println("sunucu {port} : Portta sunucu açar")
+		case "sunucu":
+			fmt.Println("sexz")
+		}
+	}
 	
 }
